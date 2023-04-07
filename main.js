be7bccda303a56f6f066f6f0e8cebb8444a5f9eb
Richard Gwati <haggio0844@gmail.com>
2023-04-07 19:26:36 +0200

    feat: drawing lines

--- main.js
+++ main.js
@@ -1,26 +1,41 @@
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const camera = new THREE.PerspectiveCamera(
+	45,
+	window.innerWidth / window.innerHeight,
+	1,
+	500,
+);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
 
-camera.position.z = 5;
+const scene = new THREE.Scene();
 
-function animate() {
-	requestAnimationFrame( animate );
- 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+// create a blueLine Basic material
+const material = new THREE.LineBasicMaterial({
+	color: 0xffffff,
+	linewidth: 1,
+	scale: 1,
+	dashSize: 30,
+	gapSize: 100,
+});
 
-	renderer.render( scene, camera );
-}
+// creating a geometry object
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
 
-animate();
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+const line = new THREE.Line(geometry, material);
+
+//adding the line to the scene
+scene.add(line);
+
+// rendering the scene
+renderer.render(scene, camera);
