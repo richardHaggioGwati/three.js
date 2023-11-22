ca9469e339151c574948148745ec1a9e08a2d57d
richardHaggioGwati <haggiog0844@gmail.com>
2023-11-22 18:37:31 +0200

    3D cube

--- js/main.js
+++ js/main.js
@@ -1,26 +1,38 @@
 import * as THREE from 'three';
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+renderer.setSize(window.innerWidth, window.innerHeight)
 
-camera.position.z = 5;
+document.body.appendChild(renderer.domElement)
 
-function animate() {
-    requestAnimationFrame(animate);
+const scene = new THREE.Scene()
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+)
 
-    renderer.render(scene, camera);
+const orbit = new OrbitControls(camera, renderer.domElement)
+
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
+
+camera.position.set(0, 2, 5)
+orbit.update()
+
+const boxGeometry = new THREE.BoxGeometry()
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00})
+const box = new THREE.Mesh(boxGeometry, boxMaterial)
+scene.add(box)
+
+function animate(time) {
+    box.rotation.x = time / 1000
+    box.rotation.y = time / 1000
+    renderer.render(scene, camera)
 }
 
-animate();
+renderer.setAnimationLoop(animate)
